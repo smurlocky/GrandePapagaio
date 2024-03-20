763ed432e127e83d9b4d1ac10e9be10bea27a10a
Marcos Vinicius <mvinicius.avelino@hotmail.com>
2024-03-19 22:57:17 -0300

    implementa metodos da classe mural

--- src/Mural.java
+++ src/Mural.java
@@ -16,4 +16,36 @@ public class Mural {
     return users;
   }
 
+  public User getUser(int index) {
+    return users.get(index);
+  }
+
+  public void removeUser(User user) {
+    users.remove(user);
+  }
+
+  public void removeUser(String name) {
+    users.remove(new User(name));
+  }
+
+  public void removeUser(int index) {
+    users.remove(index);
+  }
+
+  public List<String> getSeguidos(User user) {
+    for (User u : users) {
+      if (u.equals(user))
+        return u.getSeguidos();
+    }
+    return null;
+  }
+
+  public List<String> getTweets(User user) {
+    for (User u : users) {
+      if (u.equals(user))
+        return u.getTweets();
+    }
+    return null;
+  }
+
 }
